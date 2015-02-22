288d1d5bf2306afe96a79bd8c215d20b77be3766
17JamesH <jameshe424@gmail.com>
2015-02-21 20:21:20 -0800

    Added libraries to servo sorting project
    Imported from last year's libraries because this year's libraries caused
    errors.

---
+++ Botball Regionals/LinkCameraTest/newmenu.h
@@ -0,0 +1,15 @@
+#ifndef _NEWMENU
+#define _NEWMENU
+#include "generic.h"
+
+#define DEFAULT_OPTION 0
+extern int currstate;//defined in generic.h
+
+struct menuitem{
+	int snum;
+	char* name;
+};
+void Get_Mode();
+//other functions are hidden- just use Get_Mode();
+
+#endif
