12d670865325c21ee6ba1dc4fabfa09346c42c86
17JamesH <jameshe424@gmail.com>
2015-03-06 21:01:34 -0800

    LinkDrive project created
    Created rough sketch of the drive path. Credit to Noah for writing the
    code.

---
+++ Botball Regionals/Link/LinkDrive/main.c
@@ -0,0 +1,18 @@
+#include "drive.h"
+#include "generic.h"
+int main()
+{
+	forward(120);
+	left(50,0);
+	forward(60);
+	right(50,0);
+	forward(45);
+	right(180,0);
+	forward(240);
+	left(135,0);
+	forward(120);
+	right(45,0);
+	forward(30);
+	right(90,0);
+	forward(100);
+}
