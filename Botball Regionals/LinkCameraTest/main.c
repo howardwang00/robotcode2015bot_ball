7bf7c463185f46b3f06ae909bd4f56f0380b2cde
17JamesH <jameshe424@gmail.com>
2015-03-06 19:44:43 -0800

    Fixed camera sorting
    Finally created a rough camera sorting program. Technically works, but
    values may need to be adjusted. In addition, the program itself may
    likely need to be updated. Ignore the generic.c update, nothing was
    changed.

--- Botball Regionals/LinkCameraTest/main.c
+++ Botball Regionals/LinkCameraTest/main.c
@@ -22,8 +22,8 @@ Value:
 #define SERV_SORT 0
 #define MOT_PICK 0
 //Position Functions
-void sort_main(){set_servo_position(SERV_SORT,1833);}
-void sort_sec(){set_servo_position(SERV_SORT,640);}
+void sort_main(){set_servo_position(SERV_SORT,1833);msleep(500);}
+void sort_sec(){set_servo_position(SERV_SORT,640);msleep(500);}
 
 /*
 sorts the poms into their respective bins for time seconds
@@ -35,22 +35,21 @@ time: the duration for which this program runs
 */
 void cam_sort(int mainColor, int size, int discrepancy, int time)
 {
-	printf("blah1");
 	camera_update();
-	printf("blah");
 	float startTime = curr_time();
-	printf("blah");
 	int area = 0;
 	while(startTime+time>=curr_time())	//Timekeeper
 	{
 		camera_update();
-		printf("works pre-check");
-		area = get_object_area(mainColor,0);
-		printf("the check works");
-		if(area>=size-discrepancy&&area<=size+discrepancy)
-			sort_main();
-		else
-			sort_sec();
+		if(get_object_count(0)>0)
+		{
+			area = get_object_area(mainColor,0);
+			printf("test a");
+			if(area>=size-discrepancy&&area<=size+discrepancy)
+				sort_main();
+			else
+				sort_sec(); printf("%d",area);
+		}
 	}
 }
 int main()
@@ -58,11 +57,7 @@ int main()
 	camera_open(LOW_RES);
 	enable_servos();
 	motor(MOT_PICK,100);
-	//msleep(100000);
-	printf("test1");
 	cam_sort(0,1000,100,10);
-	msleep(1000);
-	printf("test2");
 	msleep(1000);
 	disable_servos();
 	return 0;
