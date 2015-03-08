a39ef90542e2f4a59630bf47a014cfc4ac1d4765
Henry Li <2001henryli@gmail.com>
2015-03-07 20:03:40 -0800

    added start function to link
    added start function for tournament

--- Botball Regionals/Link/Link_Main.c
+++ Botball Regionals/Link/Link_Main.c
@@ -1,7 +1,18 @@
 // Created on Fri February 6 2015
 
+#define LIGHT_SENSOR_PORT ? // light sensor port is currently unknown
+
+void start(int light_sensor_port);
+
 int main()
 {
-	printf("Hello, World!\n");
+	start(LIGHT_SENSOR_PORT);
 	return 0;
+}
+
+void start(int light_sensor_port)
+{
+	wait_for_light(light_sensor_port);
+	shut_down_in(119.5); // shuts down in 119.5 seconds instead of 120 so the robot can have time to shut down
+	enable_servos();
 }
