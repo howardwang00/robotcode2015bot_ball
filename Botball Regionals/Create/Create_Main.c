31ccf87fca599fe601a5eee5302ccb6deb93e21e
Howard Wang <howardwang00@gmail.com>
2015-03-20 19:36:41 -0700

    Documented code for documentation

--- Botball Regionals/Create/Create_Main.c
+++ Botball Regionals/Create/Create_Main.c
@@ -10,10 +10,10 @@
 
 
 #define main_arm_up 1475	//on top of mesa
-#define main_arm_mid 1000	//driving
+#define main_arm_mid 1000	//position of arm when driving
 #define main_arm_down 835	//picking up cubes
 #define claw_hold_cubes 475
-#define claw_open_regular 600
+#define claw_open_regular 600	//when the claw is not holding anything
 #define claw_hold_botguy 340	//holding botguy
 #define claw_hold_pod 400	//holding pod
 
@@ -25,22 +25,15 @@ int main()
 {
 	start_function(light_start_sensor);
 	
-	create_move(-100, -100, 300);
-	
+	set_servo_position(main_arm_servo, main_arm_mid); //move arm to a higher position than cubes
+	msleep(100);
+	create_drive_direct_dist(-10, -10, 10);	//get off the wall
 	create_stop();
+	msleep(500);
 	
-	create_move(-100, 100, 300);
+	create_left(10, 0, 100); //turn to face cubes
+	msleep(10000);
 	
-	//set_servo_position(main_arm_servo, main_arm_mid); //move arm to a higher position than cubes
-	//msleep(100);
-	//create_drive_direct_dist(-10, -10, 10);
-	//msleep(60000);
-	//create_stop();
-	//msleep(500);
-	
-	//create_left(10, 0, 100); //turn to face cubes
-	//msleep(10000);
-	/*
 	set_servo_position(main_arm_servo, main_arm_down+30);
 	msleep(200);
 	set_servo_position(main_arm_servo, main_arm_down);
@@ -49,19 +42,19 @@ int main()
 	msleep(100);
 	set_servo_position(main_arm_servo, main_arm_up);
 	msleep(20000); // wait 20 seconds for Link to pass
-	*/
 	
-	/*create_drive_direct_dist(-100, -100, 100);
+	
+	create_drive_direct_dist(-100, -100, 100);
 	msleep(100);
 	create_drive_direct_dist(-30, -30, 100); //go slower for the rest of the way
 	msleep(300);
-	*/
 	
-	//set_servo_position(claw_servo, claw_open_regular); //drop cubes
 	
+	set_servo_position(claw_servo, claw_open_regular); //drop cubes
+	msleep(500);
 	
 	disable_servos();
-	//create_stop();
+	create_stop();
 	create_disconnect();
 	return 0;
 }
@@ -71,21 +64,13 @@ int main()
 void start_function(int light_start_port)
 {
 	//light_start(light_start_port_or_function_start);
-	shut_down_in(119.5); // Time is 120 seconds, but it needs to shut down a bit earlier
+	shut_down_in(119); // Time is 120 seconds, but it needs to shut down a bit earlier
 	enable_servos();
 	create_connect();
 }
 
 
-void create_move(int left_power, int right_power, int mm) {
-	create_drive_direct(right_power, left_power);
-	set_create_distance(0);
-	while(get_create_distance() < mm) {
-		msleep(1);
-	}
-}
-/*
-void create_turn(int speed) {	//note: positive speed for clockwise, negative for counterclockwise
+void create_move(int left_power, int right_power, int mm) {	//create moving forward
 	create_drive_direct(right_power, left_power);
 	set_create_distance(0);
 	while(get_create_distance() < mm) {
@@ -93,7 +78,21 @@ void create_turn(int speed) {	//note: positive speed for clockwise, negative for
 	}
 }
 
+void create_turn_CW(int speed, int degrees) {	//create turning in place clockwise
+	create_spin_CW(speed);
+	set_create_normalized_angle(0);
+	while(get_create_normalized_angle() < -degrees) {
+		msleep(1);
+	}
+}
 
+void create_turn_CCW(int speed, int degrees) {	//create turnign in place counter-clockwise
+	create_spin_CCW(speed);
+	set_create_normalized_angle(0);
+	while(get_create_normalized_angle() < degrees) {
+		msleep(1);
+	}
+}
 
 
 
