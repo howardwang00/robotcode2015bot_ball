5aacabccc1683a1c041b568746343d3292e16508
17JamesH <jameshe424@gmail.com>
2015-03-30 15:26:57 -0700

    Used state system
    Added state system to the linkdrive project for easier testing.

--- Botball Regionals/Link/LinkDrive/main.c
+++ Botball Regionals/Link/LinkDrive/main.c
@@ -31,9 +31,9 @@ Value:
 void sort_main(){set_servo_position(SERV_SORT,1500);msleep(200);}
 void sort_sec(){set_servo_position(SERV_SORT,780);msleep(200);}
 void sort_mid(){set_servo_position(SERV_SORT,1090);msleep(200);}
-void grab_poms(){slow_servo(SERV_GRAB,1000);msleep(200);}
-void release_poms(){slow_servo(SERV_GRAB,220);msleep(200);}
-void slow_servo(int servo,int pos)
+void grab_poms(){servo_set(SERV_GRAB,1000,.2);}
+void release_poms(){servo_set(SERV_GRAB,220,.2);msleep(200);}
+/*void slow_servo(int servo,int pos)
 {
 	if(pos > get_servo_position(servo))
 	{
@@ -51,13 +51,12 @@ void slow_servo(int servo,int pos)
 			msleep(20);
 		}
 	}
-}
+}*/
 void bump_poms()
 {
 	/*set_servo_position(SERV_GRAB,1500);
 	msleep(200);*/
-	slow_servo(SERV_GRAB,800);
-	msleep(200);
+	servo_set(SERV_GRAB,800,.2);
 	motor(MOT_LEFT,50);
 	motor(MOT_RIGHT,50);
 	clear_all_drive();
@@ -73,13 +72,13 @@ void squareup(int max_time)
 	while(!LEFT_BUMP&&!RIGHT_BUMP&&start_time+max_time>=curr_time())
 	{
 		if(!LEFT_BUMP)
-			motor(MOT_LEFT,-50);
+		motor(MOT_LEFT,-50);
 		else
-			motor(MOT_LEFT,0);
+		motor(MOT_LEFT,0);
 		if(!RIGHT_BUMP)
-			motor(MOT_RIGHT,-50);
+		motor(MOT_RIGHT,-50);
 		else
-			motor(MOT_RIGHT,0);
+		motor(MOT_RIGHT,0);
 	}
 	ao();
 }
@@ -109,9 +108,9 @@ void cam_sort(int mainColor, int initial, int fudge, int time, int jamDist)
 {
 	//initialization process
 	if(initial<0||initial>100) 
-		printf("Warning: Initial is out of the specified range\n");
+	printf("Warning: Initial is out of the specified range\n");
 	if(fudge<0||fudge>100)
-		printf("Warning: Fudge is out of the specified range\n");
+	printf("Warning: Fudge is out of the specified range\n");
 	int size;
 	jamDist = jamDist*CMtoBEMF;
 	float lastTest = curr_time();
@@ -122,16 +121,16 @@ void cam_sort(int mainColor, int initial, int fudge, int time, int jamDist)
 	switch(CAM_RES)
 	{
 		case LOW_RES:
-			res_val = (160*120)/100;
-			printf("Low res\n");
+		res_val = (160*120)/100;
+		printf("Low res\n");
 		break;
 		case MED_RES:
-			res_val = (320*240)/100;
-			printf("Medium res\n");
+		res_val = (320*240)/100;
+		printf("Medium res\n");
 		break;
 		case HIGH_RES:
-			res_val = (640*480)/100;
-			printf("High res\n");
+		res_val = (640*480)/100;
+		printf("High res\n");
 		break;
 	}
 	size = res_val*initial;
@@ -166,100 +165,133 @@ void cam_sort(int mainColor, int initial, int fudge, int time, int jamDist)
 		{
 			printf("Seen Blob of Main color\n");
 			if(area>=size-discrepancy&&area<=size+discrepancy)
-				sort_main();
+			sort_main();
 			else
-				printf("Blob failed specifications\n");
+			printf("Blob failed specifications\n");
 		}
 		else
 		{
 			if(mainColor==0)
-				area = get_object_area(1,0);
+			area = get_object_area(1,0);
 			else
-				area = get_object_area(0,0);
+			area = get_object_area(0,0);
 			if(area>500)
-				printf("Seen Blob of Secondary color\n");
+			printf("Seen Blob of Secondary color\n");
 			if(area>=size-discrepancy&&area<=size+discrepancy)
-				sort_sec();
+			sort_sec();
 			else
-				printf("Blob failed specifications\n");
+			printf("Blob failed specifications\n");
 		}
 		sort_mid();
 	}
 }
-/*
-#define s_START 0
-#define s_CROSSFIELD 1
+//side programs
+#define s_SQUAREUP 101
+#define s_RAWSORT 102
+
+//main programs
+#define s_START 1
+#define s_CROSSFIELD 2
+#define s_PILE1 3
+#define s_PILE2 4
+
+#define s_END 0
 
 struct menuitem menu[]=
 {
 	{s_START,"start"},
-	{s_CROSSFIELD,"crossfield"}
+	{s_CROSSFIELD,"crossfield"},
+	{s_PILE1,"Pile 1"},
+	{s_PILE2,"Pile 2"},
+	{s_SQUAREUP,"squareup"},
+	{s_RAWSORT,"sorting"},
+	{s_END,"END"},
 };
-*/
-int main2()
-{
-	squareup(10);
-	return 0;
-}
-int main3()
-{
-	camera_open(CAM_RES);
-	enable_servos();
-	grab_poms();
-	//motor(MOT_LEFT,30);
-	//motor(MOT_RIGHT,35);
-	cam_sort(0,70,25,50,3);
-}
+
 int main()
 {
-	motor(MOT_LEFT,-70);
-	motor(MOT_RIGHT,-70);
-	msleep(1000);
-	motor(MOT_RIGHT,0);
-	motor(MOT_LEFT,0);
-	forward(6);
-	slow_servo(SERV_GRAB,1250);
-	msleep(300);
-	camera_open(CAM_RES);
-	release_poms();
-	enable_servos();
-	left(5,0);
-	forward(45);
-	grab_poms();
-	right(5,0);
-	forward(130);
-	right(92,ks/2);
-	backward(75);
-	motor(MOT_LEFT,70);
-	motor(MOT_RIGHT,74);
-	cam_sort(0,70,25,30,3);
-	release_poms();
-	backward(30);
-	forward(30);
-	right(88,0);
-	forward(60);
-	motor(MOT_PICK,-40);
-	grab_poms();
-	backward(60);
-	left(88,0);
-	release_poms();
-	//right(105,46);
-	/*motor(MOT_LEFT,-70);
-	motor(MOT_RIGHT,-70);
-	msleep(3000);
-	forward(2);
-	left(20,0);
-	forward(45);
-	right(180,0);*/
-	motor(MOT_LEFT,40);
-	motor(MOT_RIGHT,55);
-	cam_sort(0,70,30,50,3);
-	forward(240);
-	left(135,0);
-	forward(120);
-	right(45,0);
-	forward(30);
-	right(90,0);
-	forward(100);
-	return 0;
+	Get_Mode();
+	while(currstate!=s_END)
+	{
+		state(s_SQUAREUP)
+		{
+			squareup(10);
+			next(s_END);
+		}
+		state(s_RAWSORT)
+		{
+			camera_open(CAM_RES);
+			enable_servos();
+			grab_poms();
+			//motor(MOT_LEFT,30);
+			//motor(MOT_RIGHT,35);
+			cam_sort(0,70,25,50,3);
+			next(s_END);
+		}
+		state(s_START)
+		{
+			motor(MOT_LEFT,-70);
+			motor(MOT_RIGHT,-70);
+			msleep(1000);
+			motor(MOT_RIGHT,0);
+			motor(MOT_LEFT,0);
+			forward(6);
+			servo_set(SERV_GRAB,1250,.3);
+			camera_open(CAM_RES);
+			release_poms();
+			enable_servos();
+			next(s_CROSSFIELD);
+		}
+		state(s_CROSSFIELD)
+		{
+			left(5,0);
+			forward(60);
+			grab_poms();
+			right(5,0);
+			forward(120);
+			next(s_PILE1);
+		}
+		state(s_PILE1)
+		{
+			right(92,ks/2);
+			backward(75);
+			motor(MOT_LEFT,70);
+			motor(MOT_RIGHT,74);
+			cam_sort(0,70,25,30,3);
+			release_poms();
+			next(s_PILE2);
+		}
+		state(s_PILE2)
+		{
+			backward(30);
+			forward(30);
+			right(88,0);
+			forward(60);
+			motor(MOT_PICK,-40);
+			grab_poms();
+			backward(60);
+			left(88,0);
+			release_poms();
+			//right(105,46);
+			/*motor(MOT_LEFT,-70);
+			motor(MOT_RIGHT,-70);
+			msleep(3000);
+			forward(2);
+			left(20,0);
+			forward(45);
+			right(180,0);*/
+			motor(MOT_LEFT,40);
+			motor(MOT_RIGHT,55);
+			cam_sort(0,70,30,50,3);
+			forward(240);
+			left(135,0);
+			forward(120);
+			right(45,0);
+			forward(30);
+			right(90,0);
+			forward(100);
+			next(s_END);
+		}
+		return 0;
+	}
 }
--- Botball Regionals/Link/LinkDrive/newmenu.h
+++ Botball Regionals/Link/LinkDrive/newmenu.h
@@ -1,7 +1,6 @@
 #ifndef _NEWMENU
 #define _NEWMENU
 #include "generic.h"
-#include <string.h>
 
 #define DEFAULT_OPTION 0
 extern int currstate;//defined in generic.h
@@ -10,6 +9,7 @@ struct menuitem{
 	int snum;
 	char* name;
 };
+extern struct menuitem menu[];
 void Get_Mode();
 //other functions are hidden- just use Get_Mode();
 
