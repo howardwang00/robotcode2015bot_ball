b11153d383ce0fca118e9dc3f79b861a49e976d6
Howard Wang <howardwang00@gmail.com>
2015-03-30 17:54:32 -0700

    We can sort of move up the arm without a camera

--- Botball Regionals/Create/Create_Main.c
+++ Botball Regionals/Create/Create_Main.c
@@ -4,18 +4,17 @@
 #include "createDrive.h"
 #include "generic.h"
 
-//#define Tournament 1
 
 #define light_start_sensor 2 //random #, change later, light sensor not yet applied
 #define main_arm_servo_1 2	//KIPR flipped 2 and 3 on the servo ports
 #define main_arm_servo_2 3
-#define claw_servo 2
+#define claw_servo 0
 
 
-#define main_arm_up_servo_1 1225	//on top of mesa
+#define main_arm_up_servo_1 1100	//on top of mesa
 //#define main_arm_mid_servo_1 1280
-#define main_arm_down_servo_1 1825	//picking up cubes
-#define main_arm_default 530	//position of arm when driving
+#define main_arm_down_servo_1 1300	//picking up cubes
+#define main_arm_default 100	//position of arm when driving
 //#define main_arm_up_servo_2 
 //#define main_arm_mid_servo_2 
 //#define main_arm_down_servo_2 
@@ -23,9 +22,9 @@
 #define claw_hold_cubes 475
 #define claw_open_regular 600	//when the claw is not holding anything
 #define claw_hold_botguy 340	//holding botguy
-#define claw_hold_pod 400	//holding pod
+//#define claw_hold_pod 400	//holding pod
 
-
+//#define tournament
 void start_function(int light_start_port_for_function_start); //function to start the robot
 
 void create_move(int left_power, int right_power, int mm);
@@ -40,11 +39,11 @@ void create_default();
 
 
 
-#define MAIN
+//#define MAIN
 #ifdef MAIN
 int main()
 {
-	create_default();	//make sure servo is in the correct position so enabling servos won't screw us up
+	create_arm(main_arm_down_servo_1);	//make sure servo is in the correct position so enabling servos won't screw us up
 	printf("1\n");
 	start_function(light_start_sensor);
 	printf("4\n");
@@ -73,42 +72,48 @@ int main()
 #endif
 
 
-//#define TEST_CREATE_DRIVEPATH
+#define TEST_CREATE_DRIVEPATH
 #ifdef TEST_CREATE_DRIVEPATH
 void main()
 {
+	create_arm(main_arm_down_servo_1 - 200);
+	set_servo_position(claw_servo, claw_open_regular);
+	enable_servos();
+	create_connect();
 	
 	msleep(100);
-	create_drive_direct_dist(-10, -10, -10);	//get off the wall
+	create_drive_direct_dist(-10, -10, -5);	//get off the wall
 	create_stop();
 	create_block();
-	msleep(500);
 	
-	create_left(30, 0, 10); //turn to face cubes
+	create_left(50, 0, 15); //turn to face cubes
 	create_stop();
 	create_block();
 	msleep(100);
 	
 	
-	create_arm(main_arm_down-30);
-	msleep(200);
-	create_arm(main_arm_down);
+	create_arm(main_arm_down_servo_1);
 	msleep(300);
 	set_servo_position(claw_servo, claw_hold_cubes);
 	msleep(100);
-	create_arm(main_arm_up);
-	msleep(20000); // wait 20 seconds for Link to pass
+	create_arm(main_arm_up_servo_1);
+	msleep(1000); // wait for Link to pass
 	
 	
-	create_drive_direct_dist(-100, -100, -100);
-	create_drive_direct_dist(-30, -30, -100); //go slower for the rest of the way
+	create_drive_direct_dist(-100, -100, -400);
 	create_stop();
 	create_block();
-	
+	msleep(500);
 	
 	set_servo_position(claw_servo, claw_open_regular); //drop cubes
 	msleep(500);
 	
+	disable_servos();
+	msleep(100);
+	create_stop();
+	create_block();
+	msleep(100);
+	create_disconnect();
 }
 #endif
 
@@ -120,7 +125,7 @@ void main()
 
 void start_function(int light_start_port)
 {
-	#ifdef Tournament
+	#ifdef tournament
 	light_start(light_start_port_or_function_start);
 	#endif
 	shut_down_in(119); // Time is 120 seconds, but it needs to shut down a bit earlier
@@ -164,4 +169,5 @@ void create_default() {
 	set_servo_position(main_arm_servo_1, main_arm_default);
 	set_servo_position(main_arm_servo_2, (- main_arm_default) + main_arm_down_servo_1 + 100);
 }
+
 
